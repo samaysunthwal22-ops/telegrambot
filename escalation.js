64ad12e4527bae780e66c7da40c06d54db117319
samaysunthwal22-ops <samaysunthwal22@gmail.com>
2026-03-12 20:17:28 +0530

    Update escalation.js

--- escalation.js
+++ escalation.js
@@ -1,1 +1,25 @@
+function isEscalationMessage(message) {
+  const msg = (message || "").toLowerCase();
 
+  const triggers = [
+    "refund",
+    "scam",
+    "not working",
+    "doesn't work",
+    "does not work",
+    "ban",
+    "banned",
+    "locked",
+    "wrong account",
+    "chargeback",
+    "fake",
+    "fraud",
+    "problem",
+    "issue",
+    "angry"
+  ];
+
+  return triggers.some(word => msg.includes(word));
+}
+
+module.exports = { isEscalationMessage };
